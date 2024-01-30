0eadcd73b9e7a942d3fd05a0df9a3b12d09279c6
roberto-palanga <r.palanga94@gmail.com>
2024-01-30 18:20:07 +0100

    Mile stone 1

--- js/script.js
+++ js/script.js
@@ -9,47 +9,79 @@
 //  Questo richiederà un minimo di ricerca.
 
 
-const age = Number( prompt("quanti anni hai?"));
+// const age = Number( prompt("quanti anni hai?"));
 
-if age {
-    
-}
 
-const costPromile= 0.21;
 
-const km = Number ( prompt("indica la distanza in kilometri"));
 
-let sum = km * costPromile;
 
-console.log(sum);
+const ButtonElement = document.querySelector("#create");
+ButtonElement.addEventListener(`click`,
+    function () {
+
+        const nameElement = document.querySelector("#user-Name");
+        const kmElement = document.querySelector("#distance");
+        const AgeElement = document.getElementById("age").value;
+
+
+        
+        const costPromile = 0.21;
+        
+        let sum = kmElement.value * costPromile;
+
+        
+
+        const DiscountAge1 = 18;
+
+        const DiscountAge2 = 64;
+
+        if (AgeElement < DiscountAge1) {
+            sum = sum - (sum * 20 / 100);
+
+
+        }
+
+
+        if (AgeElement > DiscountAge2) {
+            sum = sum - (sum * 40 / 100);
+        }
+
+        
+        
+        
+
+        document.getElementById("output").innerHTML = `Il costo del Biglietto è ${sum}`;
+
+        
+    }
+
+)
+
+
+
+
+
+
+
+// const km = Number ( prompt("indica la distanza in kilometri"));
+
+// let sum = km * costPromile;
+
+
 
 
 // Sconti 
 
-const DiscountAge1 = 18;
-
-const DiscountAge2 = 64;
-
-if (age < DiscountAge1) {
-   sum = sum - (sum * 20 / 100);
 
 
-} 
-    
+// document.getElementById("distance").innerHTML = `La distanza selezionata è ${km} `;
 
-if (age > DiscountAge2){
-    sum = sum - (sum * 40 / 100);
-}
-
-document.getElementById("distance").innerHTML = `La distanza selezionata è ${km} `;
-
-document.getElementById("age").innerHTML = `La tua età è ${age} `;
-
-document.getElementById("result").innerHTML = `Il costo del Biglietto è ${sum.toFixed(2)} `;
+// document.getElementById("age").innerHTML = `La tua età è ${age} `;
 
 
 
-
-
+// 
+// Ristrutturazione del Codice 
+// 
 
 
